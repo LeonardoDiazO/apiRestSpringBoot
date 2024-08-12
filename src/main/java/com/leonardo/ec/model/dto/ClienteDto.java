4fb19b1ff4b87b30acef2b9824e5a06ec6af3c15
LeonardoDiazO <maximun_neo@hotmai.com>
2024-08-11 19:11:19 -0500

    feat: mejoras en la entidad para trabajr con DTO

---
+++ src/main/java/com/leonardo/ec/model/dto/ClienteDto.java
@@ -0,0 +1,21 @@
+package com.leonardo.ec.model.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ClienteDto implements Serializable {
+
+    private Integer idCliente;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private Date fechaRegistro;
+
+}
--- src/main/java/com/leonardo/ec/service/impl/ClienteImpl.java
+++ src/main/java/com/leonardo/ec/service/impl/ClienteImpl.java
@@ -1,11 +1,14 @@
 package com.leonardo.ec.service.impl;
 
+import com.leonardo.ec.model.dto.ClienteDto;
 import com.leonardo.ec.model.entity.Cliente;
 import com.leonardo.ec.service.IClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.leonardo.ec.model.dao.ClienteDao;
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Service
 public class ClienteImpl implements IClient {
@@ -13,18 +16,30 @@ public class ClienteImpl implements IClient {
     @Autowired
     private ClienteDao clienteDao;
 
+    @Transactional
     @Override
-    public Cliente save(Cliente cliente) {
-        return clienteDao.save(cliente);
+    public Cliente save(ClienteDto clienteDto) {
+        Cliente cliente = Cliente.builder()
+                          .idCliente(clienteDto.getIdCliente())
+                          .nombre(clienteDto.getNombre())
+                          .apellido(clienteDto.getApellido())
+                          .correo(clienteDto.getCorreo())
+                          .fechaRegistro(clienteDto.getFechaRegistro())
+                          .build();
+
+        return  clienteDao.save(cliente);
+
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Cliente findById(Integer id) {
         return clienteDao.findById(id).orElse(null);
     }
 
+    @Transactional
     @Override
     public void delete(Cliente cliente) {
-
+        clienteDao.delete(cliente);
     }
 }
